2240552a1389b31a5e1ebf68913f4db667fa46de
AbdullahAlWaraa <103293258+Abdullah963039@users.noreply.github.com>
2023-11-21 17:24:27 +0300

    add list component

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,9 +1,42 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { ListContainer } from "./_components/list-container";
+
 interface BoardIdPageProps {
   params: {
     boardId: string;
   };
 }
-const BoardIdPage = (props: BoardIdPageProps) => {
-  return <div className="py-24">BoardIdPage</div>;
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: { orgId },
+    },
+
+    include: {
+      cards: {
+        orderBy: { order: "asc" }, // Cards Order
+      },
+    },
+
+    orderBy: { order: "asc" }, // List order
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
 };
+
 export default BoardIdPage;
