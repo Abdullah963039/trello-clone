5957f8d2faccf1b6dee06b21d7e6ef8d47f3923b
AbdullahAlWaraa <103293258+Abdullah963039@users.noreply.github.com>
2023-11-24 19:20:23 +0300

    add card modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,12 +3,16 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
 
+import { useCardModal } from "@/hooks/use-card-modal";
+
 interface CardItemProps {
   index: number;
   data: Card;
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const onOpen = useCardModal((state) => state.onOpen);
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -18,6 +22,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           ref={provided.innerRef}
           role="button"
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+          onClick={() => onOpen(data.id)}
         >
           {data.title}
         </div>
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CardModal } from "@/components/modals/card-modal";
+
+export const ModalProvider = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      <CardModal />
+    </>
+  );
+};
